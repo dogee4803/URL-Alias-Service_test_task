8eb2e006123a763935174f4f94e49767a25dc869
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-06-01 22:18:43 +0800

    Templates for views

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('create/', views.CreateShortURL.as_view(), name='create'),
+    path('list/', views.URLListView.as_view(), name='list'),
+    path('deactivate/<str:short_code>/', views.DeactivateURL.as_view(), name='deactivate'),
+    path('<str:short_code>/', views.RedirectToOriginalView.as_view(), name='redirect'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class CreateShortURL(APIView):
+    def post(self, request):
+        return Response({"message": "CreateShortURL works"}, status=status.HTTP_200_OK)
+
+
+class URLListView(APIView):
+    def get(self, request):
+        return Response({"message": "URLListView works"}, status=status.HTTP_200_OK)
+
+
+class DeactivateURL(APIView):
+    def post(self, request, pk=None):
+        return Response({"message": "DeactivateURL works"}, status=status.HTTP_200_OK)
+
+
+class RedirectToOriginalView(APIView):
+    def get(self, request, short_code):
+        
+        original_url = "https://example.com"
+
+        if original_url:
+            return HttpResponseRedirect(original_url)
+        else:
+            raise Http404("URL not found")
