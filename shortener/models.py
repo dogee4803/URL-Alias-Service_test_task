c2293e34145237aa2b123a48df5d92237f8e7ee0
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-06-01 22:25:00 +0800

    Added ShortURL model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=20, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.original_url}"
