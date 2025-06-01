c0f2d0e1bff2980e90ffee9653d9caea1d975f83
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-06-01 22:34:33 +0800

    Added expiration to model

--- shortener/models.py
+++ shortener/models.py
@@ -5,6 +5,11 @@ class ShortURL(models.Model):
     original_url = models.URLField()
     short_code = models.CharField(max_length=20, unique=True)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expire_at = models.DateTimeField()
 
     def __str__(self):
         return f"{self.short_code} -> {self.original_url}"
+    
+    def is_expired(self):
+        return timezone.now() > self.expire_at
