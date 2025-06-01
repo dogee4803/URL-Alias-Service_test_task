953d7925d2e63aa5db8f242c45068cf0511ad6d9
dmitrii.kalenitskii <dogee4803@gmail.com>
2025-06-01 22:56:59 +0800

    Basic Authentfication views

--- shortener/views.py
+++ shortener/views.py
@@ -2,25 +2,38 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 
 # Create your views here.
 
 class CreateShortURL(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     def post(self, request):
         return Response({"message": "CreateShortURL works"}, status=status.HTTP_200_OK)
 
 
 class URLListView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
         return Response({"message": "URLListView works"}, status=status.HTTP_200_OK)
 
 
 class DeactivateURL(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
     def post(self, request, pk=None):
         return Response({"message": "DeactivateURL works"}, status=status.HTTP_200_OK)
 
 
 class RedirectToOriginalView(APIView):
+    permission_classes = [AllowAny]
+    
     def get(self, request, short_code):
         
         original_url = "https://example.com"
